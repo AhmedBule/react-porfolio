bd4316a408febf0697cae779246808a9fda8ea67
Ahmed Bule <ahmedbule01@gmail.com>
2020-08-19 19:28:38 -0500

    Updates

---
+++ precache-manifest.154d06e17fdc23b4799db1e8307ba359.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c69c87010b6e686661b5b23c8e240a1",
+    "url": "/react-porfolio/index.html"
+  },
+  {
+    "revision": "0f2250dd2ab1b100caf7",
+    "url": "/react-porfolio/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "a00936d48b453c11ce82",
+    "url": "/react-porfolio/static/js/2.aef9c434.chunk.js"
+  },
+  {
+    "revision": "e980a7f3cee8e03adfb625e54adfa237",
+    "url": "/react-porfolio/static/js/2.aef9c434.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f2250dd2ab1b100caf7",
+    "url": "/react-porfolio/static/js/main.3c876d39.chunk.js"
+  },
+  {
+    "revision": "def0e9548ecf728ecbe3",
+    "url": "/react-porfolio/static/js/runtime-main.ef4287ce.js"
+  }
+]);
